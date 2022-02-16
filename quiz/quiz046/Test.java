a46833edcb3d6bb66ebebce53299f071fb4402ea
GyuminGomin <ool9898@naver.com>
2022-02-16 17:36:10 +0900

    Java_License

---
+++ quiz/quiz046/Test.java
@@ -0,0 +1,9 @@
+package quiz.quiz046;
+
+public class Test {
+    public static void main(String[] args) {
+        if (args[0].equals("Hello") ? false : true) {
+            System.out.println("success");
+        } else System.out.println("Failure"); 
+    }
+}
