89b2d2b6ec371b390909620a41203e9e96145848
GyuminGomin <ool9898@naver.com>
2022-02-16 16:57:03 +0900

    Java_License

--- quiz/quiz077/Bob.java
+++ quiz/quiz077/Bob.java
@@ -20,6 +20,6 @@ public class Bob {
         Star obj3 = obj2;
         ((Sun) obj3).doStuff();
         ((Star) obj2).doStuff();
-        ((Universe) obj2).doStuff();
+        ((Universe) obj2).doStuff(); // 하나의 예시를 들어보자면, int a = 3; 변수 a를 캐스팅 시킨다고 생각하면 된다.
     }
 }
