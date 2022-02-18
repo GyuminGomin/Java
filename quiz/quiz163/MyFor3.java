d7f7ce59d817f59cd25e725b06ac825546029b4b
GyuminGomin <ool9898@naver.com>
2022-02-18 14:35:03 +0900

    Java_License

---
+++ quiz/quiz163/MyFor3.java
@@ -0,0 +1,10 @@
+package quiz.quiz163;
+
+public class MyFor3 {
+    public static void main(String[] args) {
+        int[] xx = null;
+        for (int ii : xx){
+            System.out.println(ii);
+        }
+    }
+}
---
+++ quiz/quiz166/Test.java
@@ -0,0 +1,15 @@
+package quiz.quiz166;
+
+public class Test {
+    public static void main(String[] args) {
+        Test ts = new Test();
+        System.out.println(isAvailable + " ");
+        isAvailable = ts.doStuff();
+        System.out.println(isAvailable);
+    }
+    public static boolean doStuff(){
+        return !isAvailable;
+    }
+    static boolean isAvailable = false;
+
+}
