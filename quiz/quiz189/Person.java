9c91962cbd9af707932e52909a3e2d4b3f61e1ba
GyuminGomin <ool9898@naver.com>
2022-02-18 17:27:56 +0900

    Java_License

---
+++ quiz/quiz189/Person.java
@@ -0,0 +1,20 @@
+package quiz.quiz189;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String n, int a){
+        name = n;
+        age = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    
+}
