62bd043b848e76b8b4fabd767140fc4290474e1c
GyuminGomin <ool9898@naver.com>
2023-09-21 14:59:08 +0900

    abstract & interface in java

---
+++ src/chap04/Clothing.java
@@ -0,0 +1,23 @@
+package chap04;
+
+public class Clothing extends Product implements Sellable{
+    
+    private String size;
+
+    public Clothing(String name, double price, String size) {
+        super(name, price);
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Price: " + price + ", Size: " + size + ")";
+    }
+
+    @Override
+    public double sell(int quantity) {
+        return price * quantity;
+    }
+
+    
+}
---
+++ src/chap04/Sellable.java
@@ -0,0 +1,5 @@
+package chap04;
+
+public interface Sellable {
+    double sell(int quantity);
+}
