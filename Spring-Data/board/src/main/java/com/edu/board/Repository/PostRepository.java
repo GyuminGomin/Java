fcb27abc11d051069dcea9db80a7d04bcaa388cd
GyuminGomin <ool9898@naver.com>
2023-10-10 21:09:43 +0900

    edit repository

--- Spring-Data/board/src/main/java/com/edu/board/Repository/PostRepository.java
+++ Spring-Data/board/src/main/java/com/edu/board/Repository/PostRepository.java
@@ -2,8 +2,8 @@ package com.edu.board.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.edu.board.Domain.User;
+import com.edu.board.Domain.Post;
 
-public interface PostRepository extends JpaRepository<User, String>{
+public interface PostRepository extends JpaRepository<Post, Long>{
     
 }
--- Spring-Data/board/src/main/java/com/edu/board/Repository/UserRepository.java
+++ Spring-Data/board/src/main/java/com/edu/board/Repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edu.board.Domain.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, String>{
     
 }
