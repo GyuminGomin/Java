58e8f2a14d74f4671d9a25f515cafe4d4bb75d7d
GyuminGomin <ool9898@naver.com>
2023-10-29 17:57:27 +0900

    2 동작원리 이해

--- Lab1.단위마이크로서비스 구현 및 동작원리 이해/Lv2MicroservicesUnderstandingaMicroservicecode2-main-dad954d1b4fdeb80aa6bb3cfb876136103ae891a/demo/src/main/java/com/example/demo/Product.java
+++ Lab1.단위마이크로서비스 구현 및 동작원리 이해/Lv2MicroservicesUnderstandingaMicroservicecode2-main-dad954d1b4fdeb80aa6bb3cfb876136103ae891a/demo/src/main/java/com/example/demo/Product.java
@@ -1,1 +1,36 @@
+package com.example.demo;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity // Entity 표시
+// Product 클래스 생성
+public class Product {
+    
+    @Id @GeneratedValue //key값 표시 , id값 자동 생성
+    Long id;
+    String name;
+    int stock;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    
+    
+}
--- Lab1.단위마이크로서비스 구현 및 동작원리 이해/Lv2MicroservicesUnderstandingaMicroservicecode2-main-dad954d1b4fdeb80aa6bb3cfb876136103ae891a/demo/src/main/java/com/example/demo/ProductRepository.java
+++ Lab1.단위마이크로서비스 구현 및 동작원리 이해/Lv2MicroservicesUnderstandingaMicroservicecode2-main-dad954d1b4fdeb80aa6bb3cfb876136103ae891a/demo/src/main/java/com/example/demo/ProductRepository.java
@@ -1,1 +1,8 @@
+package com.example.demo;
 
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    // Entity의 Repository패턴으로 CRUD에 준하는 rest api가 자동 생성
+    
+}
