4e1269f529c98cd55446b84400b074c4b982beab
GyuminGomin <ool9898@naver.com>
2023-10-29 21:04:16 +0900

    lv2

---
+++ Lv2-동기호출방식의연동/Lv2RequestResponsecommunication-main-4ebd61ce7e540565b98f8bfd20fc0eff22fedee4/monolith/src/main/java/labshopmonolith/domain/Order.java
@@ -0,0 +1,46 @@
+package labshopmonolith.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import labshopmonolith.MonolithApplication;
+import labshopmonolith.domain.OrderPlaced;
+import lombok.Data;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String productId;
+    private Integer qty;
+    private String customerId;
+    private Double amount;
+
+    @PostPersist
+    public void onPostPersist() {
+        labshopmonolith.external.DecreaseStockCommand decreaseStockCommand = new labshopmonolith.external.DecreaseStockCommand();
+    
+          // 주문수량 정보를 커맨드 객체에 적재한다. 
+        decreaseStockCommand.setQty(getQty()); 
+        
+          // InventoryService Proxy를 통해 커맨드 객체와 함께 원격호출 한다.
+        MonolithApplication.applicationContext
+            .getBean(labshopmonolith.external.InventoryService.class)
+            .decreaseStock((Long.valueOf(getProductId())), decreaseStockCommand);
+    }
+
+    @PrePersist
+    public void onPrePersist() {}
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = MonolithApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
