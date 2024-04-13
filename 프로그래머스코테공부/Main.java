8cd4e3cce45e3d6b2da073b97d3348b0fec48d0c
gyumingomin <ool9898@naver.com>
2024-04-13 19:39:47 +0900

    퀵과 병합 정리 하자 나중에

--- 프로그래머스코테공부/Main.java
+++ 프로그래머스코테공부/Main.java
@@ -1,75 +1,112 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+
 
 class Solution {
-    public String solution(String X, String Y) {
-        String answer = "";
 
-        Map<Character, Integer> countX = new HashMap<>();
-        Map<Character, Integer> countY = new HashMap<>();
-
-        // X 문자열의 각 문자의 등장 횟수 세기
-        for (char c : X.toCharArray()) {
-            // 존재하지 않으면 0을 반환 하는 메서드
-            countX.put(c, countX.getOrDefault(c, 0) + 1);
+    /* 
+    // 분할 과정
+    private int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (arr[j] >= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
+        int temp = arr[i+1];
+        arr[i+1] = arr[right];
+        arr[right] = temp;
+        return i + 1;
+    }
 
-        // Y 문자열의 각 문자의 등장 횟수 세기
-        for (char c : Y.toCharArray()) {
-            countY.put(c, countX.getOrDefault(c, 0) + 1);
+    private void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(arr, left, right);
+            quickSort(arr, left, pivotIndex -1);
+            quickSort(arr, pivotIndex +1, right);
         }
+    }
+    */
 
-        // 리스트에 저장할 형태로 사용
-        List<Character> list = new ArrayList<>();
+    // 병합 과정
+    private void merge(int[] arr, int left, int mid, int right, int m) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
 
-        // countX와 countY를 비교해 작은 개수에 맞춰 리스트에 저장
-        for (Map.Entry<Character, Integer> entry : countX.entrySet()) {
-            char key = entry.getKey();
-            int countXVal = entry.getValue();
-            int countYVal = countY.getOrDefault(key, 0);
-
-            int minCount = Math.min(countXVal, countYVal);
-            for (int i=0; i<minCount; i++) {
-                list.add(key);
+        while (i <= mid && j <= right) {
+            if (arr[i] >= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
             }
         }
 
-        if (list.isEmpty()) {
-            // list 가 비어있다면
-            answer = "-1";
-            return answer;
+        while (i <= mid) {
+            temp[k++] = arr[i++];
         }
 
-        if (list.stream().allMatch(num -> num == '0')) {
-            // list가 0으로만 이루어져 있다면
-            return "0";
+        while (j <= right) {
+            temp[k++] = arr[j++];
         }
 
-        // 마지막 list에 저장된 값을 answer에 저장하는데, 가장 큰 내림차순으로 정렬
-        Collections.sort(list);
-        Collections.reverse(list);
-
-        StringBuilder sb = new StringBuilder();
-
-        for (char c : list) {
-            sb.append(c);
+        // 병합된 배열을 원래의 배열에 복사
+        for (int p = 0; p < temp.length; p++) {
+            arr[left + p] = temp[p];
         }
-        answer = sb.toString();
+    }
+
+    private void mergeSort(int[] arr, int left, int right, int m) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid, m);
+            mergeSort(arr, mid + 1, right, m);
+            merge(arr, left, mid, right, m);
+        }
+    }
+
+    public int solution(int k, int m, int[] score) {
+        
+        int answer = 0;
+
+        // 배열을 돌려서, 우선 정렬하고 m개씩 끊어서 만든 다음
+        // 내림차순 정렬 한 후, 
+        // 버블 정렬
+        /*
+        for (int i=0; i<score.length; i++) {
+            for (int j=i+1; j<score.length; j++) {
+                if (score[i] < score[j]) {
+                    int temp = score[i];
+                    score[i] = score[j];
+                    score[j] = temp;
+                }
+            }
+        }
+        */
+
+        // 퀵 정렬
+        /*
+        quickSort(score, 0, score.length-1);
+        */
+
+        // 병합정렬
+        mergeSort(score, 0, score.length - 1, m);
+
+        for (int i=m-1; i<score.length; i+=m) {
+            answer += score[i];
+        }
+
+        answer *= m;
+
         return answer;
     }
 }
-
-
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         // 테스트 케이스 추가하면서 테스트 진행
-
-        System.out.println(solution.solution("100", "2345"));
-        System.out.println(solution.solution("100", "203045"));
-        System.out.println(solution.solution("100", "123450"));
+        System.out.println(solution.solution(3, 4, new int[]{1,2,3,1,2,3,1}));
     }
 }
