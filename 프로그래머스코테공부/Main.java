412f3e6efd2fc949606f22d424ca7c7f2882eb52
gyumingomin <ool9898@naver.com>
2024-04-15 01:09:45 +0900

    값이 infinity가 되는 경우를 어떻게 해소해야하지?

--- 프로그래머스코테공부/Main.java
+++ 프로그래머스코테공부/Main.java
@@ -1,48 +1,73 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
 class Solution {
-    public int[] solution(int k, int[] score) {
-        int[] answer = {};
-        
-        // 10, 100, 20, 150, 1, 100, 200
-        // k = 3
-        /*
-         * 10 100 100 150 150 150 200
-         *     10  20 100 100 100 150
-         *         10  20  20 100 100
-         */        
-        // 10, 10, 10, 20, 20, 100, 100
 
-        // 큐는 아니다.
-        
-        List<Integer> list = new LinkedList<>();
-        // 채워져 있는지 확인 변수
-        // 하나씩 채워 질 때마다 -1;
-        int box = k;
+    private long statistic(long count1, long count2) {
 
-        answer = new int[score.length];
-        for (int i=0; i<score.length; i++) {
-            if (box != 0) {
-                list.add(score[i]);
-                // 크기 순서대로 저장되지는 않는다.
-                box--;
+        // 반환 경우의 수
+        double result = 1;
 
-                Collections.sort(list); // 오름 차순
-                answer[i] = list.get(0);
-            } else {
-                // 이제 꽉 채워 지면
-                // 리스트에 값을 추가하고
-                // 다시 정렬 후
-                // 가장 작은 크기를 비운다.
-                list.add(score[i]);
-                Collections.sort(list);
-                list.remove(0);
-                answer[i] = list.get(0);
+        // 전체 개수
+        long totalCount = count1+count2;
+
+        if (count2 == 0 || count1 == 0) {
+            return (long)result;
+        }
+
+        // 2가 2개, 1가 4개 라고 가정하면 (8) 두번 반복 6C2
+        if (count1 >= count2) {
+            for (int i=0; i<count2; i++) {
+                result *= (double)totalCount-i;
+                result /= (double)count2-i;
+            }
+        } else {
+            for (int i=0; i<count1; i++) {
+                result *= (double)totalCount-i;
+                result /= (double)count1-i;
             }
         }
+
+        return (long)result;
+    }
+
+    public long solution(int n) {
+        long answer = 0;
+
+        // 칸의 수 n = 4
+        // 1칸 또는 2칸만 뛸 수 있다.
+        /*
+         * [1, 1, 1, 1]
+         * [1, 2, 1]
+         * [2, 1, 1]
+         * [1, 1, 2]
+         * [2, 2]
+         */
+        // 방법 = 5
+        // 1234567을 나눈 나머지를 리턴
+
+        // n = 3
+        /*
+         * [1, 1, 1]
+         * [2, 1]
+         * [1, 2]
+         */
+
+        // 확률 문제인데..
+        /*
+         * 5C3 = 5*4*3 / 3!
+         */
+
+        long count1 = 0;
+        long count2 = 0;
+
+        for (long i=0; i<=n/2; i++) {
+            count2 = i; // 0, 1개, 2개
+            count1 = n - 2*i; // 5, 4, 1
+            // 여기 확률을 적용해서
+            // answer에 넣어준후
+            answer += statistic(count1, count2);
+            
+        }
+
+        answer %= 1234567;
 
         return answer;
     }
@@ -51,6 +76,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         // 테스트 케이스 추가하면서 테스트 진행
-        System.out.println(Arrays.toString(solution.solution(3, new int[]{10, 100,20, 150, 1, 100, 200}))); 
+
+        System.out.println(solution.solution(500)); 
     }
 }
