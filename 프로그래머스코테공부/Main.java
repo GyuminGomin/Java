595961c17bd21df4ccb49e4e53991803d110de57
gyumingomin <ool9898@naver.com>
2024-04-15 23:47:44 +0900

    세상은 넓고 변태는 많다..ㄷㄷ

--- 프로그래머스코테공부/Main.java
+++ 프로그래머스코테공부/Main.java
@@ -1,12 +1,30 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     
-    public int solution(int[] nums) {
+    public int solution(int n) {
         int answer = 0;
 
-        
+        // n을 이진수로 변환 시키자
+        String binaryNumber = Integer.toBinaryString(n);
+
+        while (true) {
+            n += 1;
+            String binaryNextNumber = Integer.toBinaryString(n);
+            int count = 0;
+            for (int i=0; i<binaryNumber.length(); i++) {
+                if (binaryNumber.charAt(i) == '1') {
+                    count++;
+                }
+            }
+            for (int i=0; i<binaryNextNumber.length(); i++) {
+                if (binaryNextNumber.charAt(i) == '1') {
+                    count--;
+                }
+            }
+            if (count == 0) {
+                answer = n;
+                break;
+            }
+        }
 
         return answer;
     }
@@ -16,7 +34,7 @@ public class Main {
         Solution solution = new Solution();
         // 테스트 케이스 추가하면서 테스트 진행
 
-        // System.out.println(solution.solution());
+        System.out.println(solution.solution(15));
         
     }
 }
