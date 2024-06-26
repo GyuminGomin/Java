552bdab7f2d5c34700aa1524eee13a3dbd88f4b6
GyuminGomin <ool9898@naver.com>
2024-06-26 13:28:55 +0900

    36번 소수 구하기

--- 프로그래머스코테공부/Main.java
+++ 프로그래머스코테공부/Main.java
@@ -1,33 +1,9 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 class Solution {
-    
-    public int solution(int[] priorities, int location) {
+
+    public int solution(int n) {
         int answer = 0;
 
-        // 큐에서 대기중인 프로세스 하나를 꺼내기
-        // 큐에 대기중인 프로세스 중 우선순위가 
-        // 더 높은 프로세스가 있다면 방금 꺼낸
-        // 프로세스를 다시 큐에 넣기
-        // 만약 그런 프로세스가 없다면 방금 꺼낸 프로세스 실행
-        // 한 번 실행한 프로세스는 다시 큐에 넣지 않고 그대로 종료
-
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-
-        for (int p : priorities) {
-            pq.add(p);
-        }
-
-        while (!pq.isEmpty()) {
-            System.out.println(pq.poll());
-        }
+        
 
         return answer;
     }
@@ -37,7 +13,7 @@ public class Main {
         Solution solution = new Solution();
         // 테스트 케이스 추가하면서 테스트 진행
 
-        System.out.println(solution.solution(new int[]{2,1,3,2}, 2));
+        System.out.println(solution.solution(15));
         
     }
 }
