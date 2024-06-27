e8a2fbb65ed9e2e0290cc7197f9860997769b6a8
GyuminGomin <ool9898@naver.com>
2024-06-27 17:55:41 +0900

    37번

--- 프로그래머스코테공부/Main.java
+++ 프로그래머스코테공부/Main.java
@@ -1,9 +1,28 @@
 class Solution {
 
-    public int solution(int n) {
+    public int solution(int[] nums) {
         int answer = 0;
+        int count = 0;
 
-        
+        // 3개 이상 50개 이하이므로 이렇게 돌려도 될듯..
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                for (int k=j+1; k<nums.length; k++) {
+                    count = nums[i] + nums[j] + nums[k];
+
+                    // 소수 구하는 것
+                    for (int l=2; l<=count/2; l++) {
+                        if (count % l == 0) {
+                            // 소수가 아님
+                            break;
+                        } else {
+                            if (l == count/2) answer++;
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
 
         return answer;
     }
@@ -13,7 +32,7 @@ public class Main {
         Solution solution = new Solution();
         // 테스트 케이스 추가하면서 테스트 진행
 
-        System.out.println(solution.solution(15));
+        System.out.println(solution.solution(new int[]{1,2,7,6,4}));
         
     }
 }
