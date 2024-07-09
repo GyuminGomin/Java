ad210c460ff8a4bf8e57c3d4ec90599737eee335
GyuminGomin <ool9898@naver.com>
2024-07-09 16:14:06 +0900

    38번

--- 프로그래머스코테공부/Main.java
+++ 프로그래머스코테공부/Main.java
@@ -1,28 +1,94 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class Solution {
 
-    public int solution(int[] nums) {
+    public int solution(String str1, String str2) {
         int answer = 0;
-        int count = 0;
 
-        // 3개 이상 50개 이하이므로 이렇게 돌려도 될듯..
-        for (int i=0; i<nums.length; i++) {
-            for (int j=i+1; j<nums.length; j++) {
-                for (int k=j+1; k<nums.length; k++) {
-                    count = nums[i] + nums[j] + nums[k];
+        // 자카드 유사도
+        // 97 ~ 122 -> 소문자
+        // 65 ~ 90 -> 대문자
 
-                    // 소수 구하는 것
-                    for (int l=2; l<=count/2; l++) {
-                        if (count % l == 0) {
-                            // 소수가 아님
-                            break;
-                        } else {
-                            if (l == count/2) answer++;
-                            continue;
-                        }
-                    }
-                }
+        char[] charList = str1.toCharArray();
+
+        // 이제 2개씩 끊어서 새로운 리스트에 저장
+        List<String> answerList1 = new ArrayList<>();
+        for (int i=0; i<charList.length-1; i++) {
+            if (i == charList.length-1) break;
+
+            if ( ((charList[i] >= 97 && charList[i] <=122) || (charList[i] >= 65 && charList[i] <= 90))
+            && ((charList[i+1] >= 97 && charList[i+1] <=122) || (charList[i+1] >= 65 && charList[i+1] <= 90)) ) {
+                answerList1.add(Character.toString(charList[i]).toLowerCase() + Character.toString(charList[i+1]).toLowerCase());
             }
         }
+
+        charList = str2.toCharArray();
+
+        List<String> answerList2 = new ArrayList<>();
+
+        for (int i=0; i<charList.length-1; i++) {
+            if (i == charList.length-1) break;
+
+            if ( ((charList[i] >= 97 && charList[i] <=122) || (charList[i] >= 65 && charList[i] <= 90))
+            && ((charList[i+1] >= 97 && charList[i+1] <=122) || (charList[i+1] >= 65 && charList[i+1] <= 90)) ) {
+                answerList2.add(Character.toString(charList[i]).toLowerCase() + Character.toString(charList[i+1]).toLowerCase());
+            }
+        }
+
+        double result = 0;
+        if (answerList1.size() == 0 && answerList2.size() == 0) result = 1;
+        else {
+            Map<String, Integer> map1 = new HashMap<>();
+            Map<String, Integer> map2 = new HashMap<>();
+
+            for (int i=0; i<answerList1.size(); i++) {
+                map1.put(answerList1.get(i), map1.getOrDefault(answerList1.get(i), 0)+1);   
+            }
+
+            for (int i=0; i<answerList2.size(); i++) {
+                map2.put(answerList2.get(i), map2.getOrDefault(answerList2.get(i), 0)+1);
+            }
+            
+            int intersector = 0;
+            // 교집합 개수 구하기
+            for (String key : map1.keySet()) {
+                if (map2.containsKey(key)) {
+                    int eqval = map1.get(key);
+                    int eqval2 = map2.get(key);
+
+                    intersector += Math.min(eqval, eqval2);
+                }
+            }
+
+            int union = 0;
+            // 합집합 개수 구하기
+            for (String key : map1.keySet()) {
+                if (map2.containsKey(key)) {
+                    int eqval = map1.get(key);
+                    int eqval2 = map2.get(key);
+
+                    union += Math.max(eqval, eqval2);
+                } else {
+                    union += map1.get(key);
+                }
+            }
+
+            for (String key : map2.keySet()) {
+                if (!map1.containsKey(key)) {
+                    union += map2.get(key);
+                }
+            }
+
+            // 교집합/합집합
+            result = (double)intersector/union;
+        }
+
+        result *= 65536;
+        answer = (int)Math.floor(result);
 
         return answer;
     }
@@ -32,7 +98,10 @@ public class Main {
         Solution solution = new Solution();
         // 테스트 케이스 추가하면서 테스트 진행
 
-        System.out.println(solution.solution(new int[]{1,2,7,6,4}));
-        
+        //System.out.println(solution.solution("FRANCE", "french"));
+        //System.out.println(solution.solution("E=M*C^2", "e=m*c^2"));
+        //System.out.println(solution.solution("handshake", "shake hands"));
+        //System.out.println(solution.solution("aa1+aa2", "AAAA12"));
+        System.out.println(solution.solution("AAbbaa_AAbb", "BBB"));
     }
 }
