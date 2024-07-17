ced017d5b67833095c8d2f0ae4498eaec5a0bc14
GyuminGomin <ool9898@naver.com>
2024-07-17 13:43:01 +0900

    35 retry

--- 프로그래머스코테공부/Main.java
+++ 프로그래머스코테공부/Main.java
@@ -1,95 +1,37 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
 
-    public int solution(String str1, String str2) {
-        int answer = 0;
+    // result의 최대값을 구해야 함
+    private static int result = 0;
 
-        // 자카드 유사도
-        // 97 ~ 122 -> 소문자
-        // 65 ~ 90 -> 대문자
+    private static void dfs(int[][] dungeons, boolean[] isWent , int k, int res) {
 
-        char[] charList = str1.toCharArray();
-
-        // 이제 2개씩 끊어서 새로운 리스트에 저장
-        List<String> answerList1 = new ArrayList<>();
-        for (int i=0; i<charList.length-1; i++) {
-            if (i == charList.length-1) break;
-
-            if ( ((charList[i] >= 97 && charList[i] <=122) || (charList[i] >= 65 && charList[i] <= 90))
-            && ((charList[i+1] >= 97 && charList[i+1] <=122) || (charList[i+1] >= 65 && charList[i+1] <= 90)) ) {
-                answerList1.add(Character.toString(charList[i]).toLowerCase() + Character.toString(charList[i+1]).toLowerCase());
+        // 깊이 마다 한번 던전리스트를 반복해서 방문했는지 여부 체크
+        for (int i=0; i<isWent.length; i++) {
+            if (!isWent[i] && dungeons[i][0] <= k) {
+                isWent[i] = true;
+                dfs(dungeons, isWent, k - dungeons[i][1], res + 1);
+                isWent[i] = false;
             }
         }
 
-        charList = str2.toCharArray();
+        // 모든 던전을 확인했으면 현재 결과 갱신
+        if (result < res) result = res;
+    }
 
-        List<String> answerList2 = new ArrayList<>();
+    public int solution(int k, int[][] dungeons) {
+        int answer = -1;
 
-        for (int i=0; i<charList.length-1; i++) {
-            if (i == charList.length-1) break;
+        // 최소 필요 피로도
+        // 소모 피로도
+        boolean[] isWent = new boolean[dungeons.length];
+        Arrays.fill(isWent, false);
 
-            if ( ((charList[i] >= 97 && charList[i] <=122) || (charList[i] >= 65 && charList[i] <= 90))
-            && ((charList[i+1] >= 97 && charList[i+1] <=122) || (charList[i+1] >= 65 && charList[i+1] <= 90)) ) {
-                answerList2.add(Character.toString(charList[i]).toLowerCase() + Character.toString(charList[i+1]).toLowerCase());
-            }
-        }
-
-        double result = 0;
-        if (answerList1.size() == 0 && answerList2.size() == 0) result = 1;
-        else {
-            Map<String, Integer> map1 = new HashMap<>();
-            Map<String, Integer> map2 = new HashMap<>();
-
-            for (int i=0; i<answerList1.size(); i++) {
-                map1.put(answerList1.get(i), map1.getOrDefault(answerList1.get(i), 0)+1);   
-            }
-
-            for (int i=0; i<answerList2.size(); i++) {
-                map2.put(answerList2.get(i), map2.getOrDefault(answerList2.get(i), 0)+1);
-            }
-            
-            int intersector = 0;
-            // 교집합 개수 구하기
-            for (String key : map1.keySet()) {
-                if (map2.containsKey(key)) {
-                    int eqval = map1.get(key);
-                    int eqval2 = map2.get(key);
-
-                    intersector += Math.min(eqval, eqval2);
-                }
-            }
-
-            int union = 0;
-            // 합집합 개수 구하기
-            for (String key : map1.keySet()) {
-                if (map2.containsKey(key)) {
-                    int eqval = map1.get(key);
-                    int eqval2 = map2.get(key);
-
-                    union += Math.max(eqval, eqval2);
-                } else {
-                    union += map1.get(key);
-                }
-            }
-
-            for (String key : map2.keySet()) {
-                if (!map1.containsKey(key)) {
-                    union += map2.get(key);
-                }
-            }
-
-            // 교집합/합집합
-            result = (double)intersector/union;
-        }
-
-        result *= 65536;
-        answer = (int)Math.floor(result);
-
+        // depth는 1부터 시작
+        dfs(dungeons,isWent,k,0);
+        
+        answer = result;
         return answer;
     }
 }
@@ -98,10 +40,6 @@ public class Main {
         Solution solution = new Solution();
         // 테스트 케이스 추가하면서 테스트 진행
 
-        //System.out.println(solution.solution("FRANCE", "french"));
-        //System.out.println(solution.solution("E=M*C^2", "e=m*c^2"));
-        //System.out.println(solution.solution("handshake", "shake hands"));
-        //System.out.println(solution.solution("aa1+aa2", "AAAA12"));
-        System.out.println(solution.solution("AAbbaa_AAbb", "BBB"));
+        System.out.println(solution.solution(80, new int[][]{{80,20},{50,40},{30,10}}));
     }
 }
