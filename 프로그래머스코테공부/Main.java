b1b1c9e5a4c2d57aeb228a8d2210e594fdedcd55
GyuminGomin <ool9898@naver.com>
2024-07-26 13:39:13 +0900

    50번

--- 프로그래머스코테공부/Main.java
+++ 프로그래머스코테공부/Main.java
@@ -5,15 +5,31 @@ class Solution {
     public long solution(int[] weights) {
         long answer = 0;
 
+        Arrays.sort(weights);
+
+        int dup = 1;
         // 100, 180, 360, 100, 270
-        for (int i=0; i<weights.length; i++) {
-            for (int j=i+1; j<weights.length; j++) {
-                int w1 = weights[i];
-                int w2 = weights[j];
-                if (w1*3 == w2*4 || w1*2 == w2*3 || w1 == w2*2
-                || w1 == w2 || w1*3 == w2*2 || w1*2 == w2 || w1*4 == w2*3) {
-                    answer++;
+        for (int i=0; i<weights.length-1; i++) {
+            int w1 = weights[i];
+            if (w1*2 < weights[i+1]) {
+                continue;
+            } else if (w1 == weights[i+1]) {
+                answer+=dup++;
+                continue;
+            }
+            else {
+                for (int j=i+1; j<weights.length; j++) {
+                    int w2 = weights[j];
+                    if (w1*2 < w2) {
+                        dup = 1;
+                        break;
+                    }
+                    if (w1*3 == w2*2 || w1*4 == w2*3 || w1*2 == w2 || w1 == w2) {
+                        answer+=dup;
+                        continue;
+                    }
                 }
+                dup = 1;
             }
         }
 
