a80863efd0047f613a88d284ca7486ecb67e3906
GyuminGomin <ool9898@naver.com>
2025-03-22 21:24:51 +0900

    어렵다..

--- 프로그래머스코테공부/Main.java
+++ 프로그래머스코테공부/Main.java
@@ -1,54 +1,69 @@
 import java.util.*;
 
 class Solution {
+    public int[] solution(int[] nodes, int[][] edges) {
+        int[] answer = {};
 
-    private static int resultN = Integer.MAX_VALUE; // 충분히 큰 값으로 초기화
-    private static Map<String, Integer> memo = new HashMap<>(); // 메모이제이션용 Map
+        // nodes의 개수 1~40만
+        // nodes의 원소 1 ~ 100만
+        // 간선의 길이 1~ 100만
 
-    private static void dps(int depth, int n, int m, int[][] info, int result) {
-        String key = depth + "," + n + "," + m;  // 현재 상태를 문자열로 변환 (depth, n, m)
-        if (memo.containsKey(key) && memo.get(key) <= result) {
-            return; // 이미 더 좋은 결과가 있으므로 탐색 중단
+        // 정보 저장 맵
+        Map<Integer, List<Integer>> edgesInfo = new HashMap<>();
+
+        // 값 초기화
+        for (int i=0; i<nodes.length; i++) {
+            edgesInfo.put(nodes[i], new ArrayList<Integer>());
         }
-        memo.put(key, result); // 현재 결과 저장
 
-        int[] currentInfo = info[depth];
-        int thief_x = currentInfo[0];
-        int thief_y = currentInfo[1];
+        // 최대 100만 O(n)
+        for (int i=0; i<edges.length; i++) {
+            int tmp1 = edges[i][0];
+            int tmp2 = edges[i][1];
+            edgesInfo.get(tmp1).add(tmp2);
+            edgesInfo.get(tmp2).add(tmp1);
+        }
 
-        // n 감소 시도
-        if (n > thief_x) {
-            int n_tmp = n - thief_x;
-            int result_tmp = result + thief_x;
-            if (depth + 1 < info.length) {
-                if (result_tmp < resultN) {
-                    dps(depth + 1, n_tmp, m, info, result_tmp);
+        // 메인로직 작성
+        for (int i=0; i<nodes.length; i++) {
+            int key = nodes[i];
+            List<Integer> info_list = edgesInfo.get(nodes[i]);
+            int edgesLength = info_list.size();
+
+
+
+            if (key % 2 == 0) { // 짝수
+                if (edgesLength % 2 == 1) { // 홀수
+                    // 역홀짝
+
+                } else { // 짝수
+                    // 홀짝
+
                 }
-            } else {
-                resultN = Math.min(resultN, result_tmp);
+            }
+            else { // 홀수
+                if (edgesLength % 2 == 1) { // 홀수
+                    // 홀짝
+
+                } else { // 짝수
+                    // 역홀짝
+
+                }
             }
         }
 
-        // m 감소 시도
-        if (m > thief_y) {
-            int m_tmp = m - thief_y;
-            if (depth + 1 < info.length) {
-                if (result < resultN) {
-                    dps(depth + 1, n, m_tmp, info, result);
-                }
-            } else {
-                resultN = Math.min(resultN, result);
-            }
-        }
+        return answer;
     }
 
-    public int solution(int[][] info, int n, int m) {
-        resultN = Integer.MAX_VALUE;
-        memo.clear(); // 새로운 입력마다 초기화
+    private static void evenOrOdd(int key, List<Integer> list) { // 역홀짝
+        if (key % 2 == 0) {
+            
+        }
+        
+    } 
 
-        dps(0, n, m, info, 0);
+    private static void sniffling() { // 홀짝
 
-        return resultN == Integer.MAX_VALUE ? -1 : resultN;
     }
 }
 public class Main {
@@ -56,6 +71,5 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         // 테스트 케이스 추가하면서 테스트 진행
-        System.out.println(solution.solution(new int[][]{{1,2},{2,3},{2,1}}, 4, 4)); 
     }
 }
